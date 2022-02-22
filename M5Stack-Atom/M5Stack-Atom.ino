6e6046477074845c041a141596c2dfab0a444d08
Vanpoucke Tony <46611481+TonyVpck@users.noreply.github.com>
2022-02-22 21:02:08 +0100

    Update M5Stack-Atom.ino

--- M5Stack-Atom/M5Stack-Atom.ino
+++ M5Stack-Atom/M5Stack-Atom.ino
@@ -6,26 +6,26 @@
    @version 1.0.5
    @date 2022-02-12
 
-   Projet sympatique menant sur la détection de CO²
+   Projet sympathique menant sur la détection de CO²
 
-   Pour cela, nous avons une carte M5Stack atom conncecté en Serial avec le capteur de CO² (Senseair S8).
+   Pour cela, nous avons une carte M5Stack atom conncectée en Serial avec le capteur de CO² (Senseair S8).
    Le capteur de CO² communique avec le protocol Modbus.
 
-   Après avoir reçus les infos du capteur, les led s'allumeront d'une certainne couleur.
+   Après avoir reçu les infos du capteur, les led s'allumeront d'une certaine couleur.
    Vert si CO² est bon
    Orange si CO² est moyen
-   Rouge si CO² est mauvais 
+   Rouge si CO² est mauvais
    Rouge clignotant si CO² est très mauvais.
 
    Blanc au démarrage du capteur
    Bleu au calibrage du capteur
    "Neige" noir et blanc si le capteur est en défaut (branchement ou calibrage)
-   
+
    @CC-BY-SA - 2022
 */
 
 #ifndef ARDUINO_M5Stack_ATOM
-#error "Ce code est supporter uniquement sur les cartes m5stack atom."
+#error "Ce code est supporté uniquement sur les cartes m5stack atom."
 #endif
 
 #include "SenseairS8.h"
@@ -33,55 +33,60 @@
 
 // ==============================================================================================
 // ==                                                                                          ==
-// ==                                      CONSTANTES                                          ==
+// ==                                      VARIABLES                                           ==
 // ==                                                                                          ==
 // ==============================================================================================
 
-//Définition des broches nécessaire au bon fonctionnement du code
+//Définition des broches nécessaires au bon fonctionnement du code
 #define PIN_SERIAL_RX  21 //Allant sur le port UART TxD du capteur
 #define PIN_SERIAL_TX  25 //Allant sur le port UART RxD du capteur
 
-//Définition des seuil qualité de l'air
-#define LIMITE_BASSE 300
-#define AIR_MOYEN 800
-#define AIR_MEDIOCRE 1000
-#define AIR_VICIE 1500
+//Définition des seuils de qualité de l'air
+#define LIMITE_BASSE 300                      // Si en dessous passe le capteur en erreur (air extérieur 400 PPM en moyenne)
+#define AIR_MOYEN 800                         // Capteur en vert avant cette limite de PPM
+#define AIR_MEDIOCRE 1000                     // Capteur en orange avant cette limite de PPM
+#define AIR_VICIE 1500                        // Capteur en rouge avant cette limite de PPM (au delà il clignotte)
 
-//converti directement les secondes en milisecondes.
-#define ATTENTE_CHECK_CO2 1 * 1000 //nb secondes * (milisecondes dans une secondes)
+//converti directement les secondes en millisecondes.
+#define ATTENTE_CHECK_CO2 1 * 1000    //nb secondes * (milisecondes dans une secondes)
 
 //Temps (milisecondes) nécessaire à l'utilisateur pour demander une calibration du capteur
 #define TEMPS_BTN_APPUYER 3 * 1000
 
-//Temps (milisecondes) pour clignoter si nécessaire les leds.
+//Temps (milisecondes) pour faire clignoter si nécessaire les leds.
 #define TEMPS_CLIGNOTEMENT_LED 1 * 1000
 
-//Tempo des animations du capateur (milisecondes, 15 par défaut)).
+//Tempo des animations du capteur (milisecondes, 15 par défaut)).
 #define TEMPO_ANIMATION 15
 
-//Fourchette de tolérance graphique (plus cette valeur est petite plus le changement de couleur "collera" au niveau seuil)
+//Fourchette de tolérance graphique (plus cette valeur est petite, plus le changement de couleur "collera" au niveau seuil)
 #define ANIMATION_OFFSET 150
 
 //Couleur du capteur lors d'un calibrage (0 = rouge / 100 = vert / 150 = bleu).
 #define COULEUR_CALIBRATION 150
 
-//Couleur du capteur lors d'un calibrage (0 = rouge / 100 = vert / 150 = bleu).
-#define TEMPS_DEMARRAGE 5
+//Temps de démarrage du capteur en "tour d'animations" (1 = 4secondes / 5 = 20 secondes).
+#define TEMPS_DEMARRAGE 5 
 
 // ==============================================================================================
 // ==                                                                                          ==
-// ==                                      VARIABLES                                           ==
+// ==                                      CONSTANTES                                          ==
 // ==                                                                                          ==
 // ==============================================================================================
 
 /**
-   @brief dernier temps connue du controle de co²
-
+   @brief dernier temps connu du contrôle de co²
 */
 unsigned long lastTimeCheckCO2 = 0;
-unsigned long lastBlinkLedWarning = 0;
 
+/**
+   @brief permet le clignotement des animations
+*/
 bool AnimSequence;
+
+/**
+   @brief dernier temps connu du contrôle de co²
+*/
 bool calibrationRequested;
 
 // ==============================================================================================
@@ -89,28 +94,6 @@ bool calibrationRequested;
 // ==                               PROTOTYPE DES FONCTIONS                                    ==
 // ==                                                                                          ==
 // ==============================================================================================
-
-//------------------------------- Prototype concernant le capteur -------------------------------
-/**
-   @brief Fonction émettant une requete basé sur le protocol Modbus,
-   puis ce met en attente d'une réponse provenant du capteur CO²
-   Cette fonction est un condancé des fonction 'send_Request', 'read_Response', et 'get_value'
-   Elle renvoie le taux de CO²
-
-   @return  taux de CO² ou 99 si erreur.
-*/
-// unsigned long getCO2();
-
-void sendRequestCalibration();
-
-/**
-   @brief Controle le niveau de CO²
-
-*/
-void checkCO2(unsigned long co2);
-
-//Fonction d'une boucle infinie faisant clignoter la led en cas d'erreur de branchement.
-void displayError();
 
 //------------------------------- Prototype des fonctions appellée par le programme. -------------------------------
 /**
@@ -120,10 +103,32 @@ void displayError();
 void setup();
 
 /**
-   @brief Fonction nécessaire qui est appelé en boucle par le programme après le passage dans la fonction setupe
-   Un peut comme le jeu du ping pong, le 'capteur' sera en écoute, et attend ces instruction.
+   @brief Fonction nécessaire qui est appelée en boucle par le programme après le passage dans la fonction setup
+   Un peu comme le jeu du ping pong, le 'capteur' sera en écoute, et attend ses instructions.
 */
 void loop();
+
+
+//------------------------------- Prototype concernant le capteur -------------------------------
+/**
+   @brief Fonction émettant une requête basée sur le protocol Modbus,
+   puis se met en attente d'une réponse provenant du capteur CO²
+   Cette fonction est un condensé des fonctions 'send_Request', 'read_Response', et 'get_value'
+   Elle renvoie le taux de CO²
+   @return taux de CO² ou 99 si erreur.
+*/
+
+void sendRequestCalibration();
+
+/**
+   @brief Contrôle le niveau de CO²
+*/
+void checkCO2(unsigned long co2);
+
+/**
+   @brief Fonction d'une boucle infinie en cas d'erreur de branchement.
+*/
+void displayError();
 
 
 // ==============================================================================================
@@ -132,93 +137,7 @@ void loop();
 // ==                                                                                          ==
 // ==============================================================================================
 
-void sendRequestCalibration() {
-  //Serial.println("Demande calibration");
-  byte stateLed = LOW;
-
-  while (senseairS8.progressCalibration()) {
-    //Vérification si des erreur ont été détecter pendant le calibrage.
-    if (senseairS8.checkError()) {
-      displayError();
-    } else {
-      //Animation du capteur durant le calibrage.
-      for (int i = 100; i < 200; i++) {
-        for (int j = 0; j < 25; j++) {
-          M5.dis.drawpix( j, CHSV( COULEUR_CALIBRATION, 255, i));
-        }
-        delay(TEMPO_ANIMATION);
-      }
-      for (int i = 200; i > 100; i--) {
-        for (int j = 0; j < 25; j++) {
-          M5.dis.drawpix( j, CHSV( COULEUR_CALIBRATION, 255, i));
-        }
-        delay(TEMPO_ANIMATION);
-      }
-    }
-  }
-  Serial.println("Calibration terminée sans erreurs.");
-}
-
-void checkCO2(unsigned long co2) {
-
-  Serial.print("Qualité de l'air : ");
-
-  if (co2 < LIMITE_BASSE) {
-    //Mettre les led dans un état de clignotement à l'infinie si une erreur est survenue.
-    Serial.println("[Erreur de calibrage capteur]");
-    displayError();
-
-  } else if (co2 >= LIMITE_BASSE && co2 < (AIR_MOYEN - ANIMATION_OFFSET) ) {
-    //Permet d'afficher toute les led du m5stack atom en vert
-    for (int i = 0; i < 25; i++) {
-      M5.dis.drawpix( i, CHSV( 100, 255, 255));
-    }
-    Serial.println("Excellent.");
-
-  } else if (co2 >=  (AIR_MOYEN - ANIMATION_OFFSET) && co2 < (AIR_MEDIOCRE + ANIMATION_OFFSET) ) {
-    //Permet d'afficher toute les led du m5stack atom en orange
-
-    int Co2Scale = map(co2, (AIR_MOYEN - ANIMATION_OFFSET), (AIR_MEDIOCRE + ANIMATION_OFFSET), 100, 0);
-
-    for (int i = 0; i < 25; i++) {
-      M5.dis.drawpix( i, CHSV( Co2Scale, 255, 255));
-    }
-    Serial.println("Moyen.");
-
-  } else if (co2 >= (AIR_MEDIOCRE + ANIMATION_OFFSET) && co2 < AIR_VICIE) {
-    //Permet d'afficher toute les led du m5stack atom en rouge
-    for (int i = 0; i < 25; i++) {
-      M5.dis.drawpix( i, CHSV( 0, 255, 255));
-    }
-    Serial.println("Mediocre.");
-
-  } else {
-    Serial.println("Vicié.");
-    //Permet d'afficher une animation de led rouge cligonttante (seuil haut dépassé).
-    if (AnimSequence == 1) {
-      AnimSequence = 0;
-      for (int i = 0; i < 25; i++) {
-        M5.dis.drawpix( i, CHSV( 0, 0, 0));
-      }
-    }
-    else if (AnimSequence == 0) {
-      AnimSequence = 1;
-      for (int i = 0; i < 25; i++) {
-        M5.dis.drawpix( i, CHSV( 0, 255, 255));
-      }
-    }
-  }
-}
-
-void displayError() {
-  while (1) {
-    //Permet d'afficher une animation infinie d'erreur (nécéssitera de redébrancher le capteur).
-    int PixNum = random(0, 25);
-    int NoisePix = random(20, 200);
-    M5.dis.drawpix( PixNum, CHSV( 0, 0, NoisePix));
-    delay(TEMPO_ANIMATION);
-  }
-}
+// ======================== CONFIGURATION CAPTEUR : VOID SETUP ======================== //
 
 void setup() {
   // ports série et de communication capteur
@@ -243,14 +162,16 @@ void setup() {
   }
 }
 
+// ======================== FONCTIONNEMENT CAPTEUR : VOID LOOP ======================== //
 
 void loop() {
 
-  //Récupération du taux de CO² toutes les x secondes définis sur la constante 10000
-  //millis actuelle - dernier temps connus >= 10000
+  //Récupération du taux de CO² toutes les x secondes défini sur la constante 10000
+  //milliseconde actuelle - dernier temps connu >= 10000
   if (millis() - lastTimeCheckCO2 >= ATTENTE_CHECK_CO2 && !calibrationRequested) {
     //Récupération du taux de CO²
     unsigned long co2 = senseairS8.reqCO2();
+    Serial.println();
     Serial.println("CO² = " + String(co2));
 
     //Controle du co2
@@ -263,12 +184,116 @@ void loop() {
     calibrationRequested = 0;
   }
 
-  //Cette instruction met à jour l'état du boutton.
+  //Cette instruction met à jour l'état du bouton.
   M5.update();
 
-  //Si le boutton est appuyer pendant plus de x secondes alors, nous demandons une calibration du capteur.
+  //Si le bouton est appuyé pendant plus de x secondes, alors nous demandons une calibration du capteur.
   if (M5.Btn.pressedFor(TEMPS_BTN_APPUYER) && !calibrationRequested) {
     Serial.println("Calibration du capteur demandée");
     calibrationRequested = 1;
+  }
+}
+
+// ======================== CALIBRATION DU CAPTEUR CO2 ======================== //
+
+void sendRequestCalibration() {
+ 
+  byte stateLed = LOW;
+
+  while (senseairS8.progressCalibration()) {
+    //Vérification si des erreurs ont été détectées pendant le calibrage.
+    if (senseairS8.checkError()) {
+      displayError();
+    } else {
+      //Animation du capteur durant le calibrage.
+      for (int i = 100; i < 200; i++) {
+        for (int j = 0; j < 25; j++) {
+          M5.dis.drawpix( j, CHSV( COULEUR_CALIBRATION, 255, i));
+        }
+        delay(TEMPO_ANIMATION);
+      }
+      for (int i = 200; i > 100; i--) {
+        for (int j = 0; j < 25; j++) {
+          M5.dis.drawpix( j, CHSV( COULEUR_CALIBRATION, 255, i));
+        }
+        delay(TEMPO_ANIMATION);
+      }
+    }
+  }
+  Serial.println("Calibration terminée sans erreur.");
+}
+
+// ======================== INTERPRETATION DU CAPTEUR CO2 ======================== //
+
+void checkCO2(unsigned long co2) {
+
+  Serial.print("Qualité de l'air : ");
+
+  // ============ CAPTEUR : EN ERREUR // code couleur : "neige" noir et blanc ==== //
+
+  if (co2 < LIMITE_BASSE) {
+    //Mettre les leds dans un état de clignotement à l'infini si une erreur est survenue.
+    Serial.println("[Erreur de calibrage capteur]");
+    displayError();
+
+    // ============ CAPTEUR : AIR EXCELLENT // code couleur : vert ==== //
+
+  } else if (co2 >= LIMITE_BASSE && co2 < (AIR_MOYEN - ANIMATION_OFFSET) ) {
+    //Permet d'afficher toutes les leds du m5stack atom en vert
+    for (int i = 0; i < 25; i++) {
+      M5.dis.drawpix( i, CHSV( 100, 255, 255));
+    }
+    Serial.println("Excellent.");
+
+    // ============ CAPTEUR : AIR MOYEN // code couleur : orange ==== //
+
+  } else if (co2 >=  (AIR_MOYEN - ANIMATION_OFFSET) && co2 < (AIR_MEDIOCRE + ANIMATION_OFFSET) ) {
+    //Permet d'afficher toutes les leds du m5stack atom en orange
+
+    int Co2Scale = map(co2, (AIR_MOYEN - ANIMATION_OFFSET), (AIR_MEDIOCRE + ANIMATION_OFFSET), 100, 0);
+
+    for (int i = 0; i < 25; i++) {
+      M5.dis.drawpix( i, CHSV( Co2Scale, 255, 255));
+    }
+    Serial.println("Moyen.");
+
+    // ============ CAPTEUR : AIR MAUVAIS // code couleur : rouge ==== //
+
+  } else if (co2 >= (AIR_MEDIOCRE + ANIMATION_OFFSET) && co2 < AIR_VICIE) {
+    //Permet d'afficher toutes les leds du m5stack atom en rouge
+    for (int i = 0; i < 25; i++) {
+      M5.dis.drawpix( i, CHSV( 0, 255, 255));
+    }
+    Serial.println("Médiocre.");
+
+    // ============ CAPTEUR : AIR TRES MAUVAIS // code couleur : rouge clignotant ==== //
+
+  } else {
+    //Permet d'afficher une animation de led rouge clignotante (seuil haut dépassé).
+    if (AnimSequence == 1) {
+      AnimSequence = 0;
+      for (int i = 0; i < 25; i++) {
+        M5.dis.drawpix( i, CHSV( 0, 0, 0));
+      }
+    }
+    else if (AnimSequence == 0) {
+      AnimSequence = 1;
+      for (int i = 0; i < 25; i++) {
+        M5.dis.drawpix( i, CHSV( 0, 255, 255));
+      }
+    }
+    Serial.println("Vicié.");
+  }
+}
+
+// ======================== ERREUR DU CAPTEUR CO2 ======================== //
+
+void displayError() {
+  while (1) {
+    //Permet d'afficher une animation infinie d'erreur (nécéssitera de redébrancher le capteur).
+    int PixNum = random(0, 25);
+    int NoisePix = random(20, 200);
+    M5.dis.drawpix( PixNum, CHSV( 0, 0, NoisePix));
+    delay(TEMPO_ANIMATION);
   }
 }
